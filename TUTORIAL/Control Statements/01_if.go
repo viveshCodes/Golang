4a0330ea78f92a1e06648b3dfcd24cd3436ea549
Vivesh Yadav <viveshofficial@gmail.com>
2020-04-05 15:23:39 +0545

    Add programs for control statements

---
+++ TUTORIAL/Control Statements/01_if.go
@@ -0,0 +1,15 @@
+package main
+import(
+    "fmt"
+)
+ func main(){
+     jersy :=7
+     
+     if jersy== 7 {
+         fmt.Println("The jersy belongs to MS Dhoni")
+     } else if jersy==8{
+         fmt.Println("The jersy belongs to Jadeja")
+     }else{
+         fmt.Println("The jersy belongs to neither Mahi bahi nor Jaddu")
+     }
+ }
---
+++ TUTORIAL/Control Statements/02_switch.go
@@ -0,0 +1,16 @@
+package main
+import(
+    "fmt"
+)
+ func main(){
+     switch trophyYear:=2011 ; trophyYear{
+       case 2007 , 2011 , 2013 :
+       fmt.Println("MS Dhoni won ICC trophy for India.")
+       case 1983 :
+       fmt.Println("Kapil Dev won ICC trophy for India.")
+       default :
+       fmt.Println("Broken Hearts.")
+       
+     }
+     
+ }
