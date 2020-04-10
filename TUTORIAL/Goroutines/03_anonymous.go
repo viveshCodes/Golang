0bcc4d3f2d7354df3d8f6cb4c8e2a3cf40709a8a
Vivesh Yadav <viveshofficial@gmail.com>
2020-04-10 16:38:07 +0545

    Add another anonymous goroutine

--- TUTORIAL/Goroutines/03_anonymous.go
+++ TUTORIAL/Goroutines/03_anonymous.go
@@ -5,9 +5,10 @@ import (
 )
 func main(){
 	message := "Hello World , Stay Safe"
-	go func(){
-		fmt.Println(message)
-	}()
+	go func(msg string){
+		fmt.Println(msg)
+	}(message)
 	message = "Hello World , It's Lockdown"
 	time.Sleep(100 * time.Millisecond)
 }
+/*Hello World , Stay Safe*/
--- TUTORIAL/Goroutines/04_anonymous_can_go_wrong.go
+++ TUTORIAL/Goroutines/04_anonymous_can_go_wrong.go
@@ -1,13 +1,19 @@
 package main
+
 import (
 	"fmt"
 	"time"
 )
-func main(){
+
+func main() {
 	message := "Hello World , Stay Safe"
-	go func(){
+	go func() {
 		fmt.Println(message)
 	}()
 	message = "Hello World , It's Lockdown"
 	time.Sleep(100 * time.Millisecond)
 }
+
+/*output:
+Hello World , It's Lockdown
+*/
