205a536dbfd44445a9b73d0be61de1df98bcb8a1
Vivesh Yadav <viveshofficial@gmail.com>
2020-04-10 18:42:10 +0545

    Add rwmutex concept

--- main.go
+++ main.go
@@ -1,18 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"runtime"
+	"sync"
 )
 
+var wg = sync.WaitGroup{}
+var counter = 0
+var m = sync.RWMutex{}
+
 func main() {
-	fmt.Println("Threads : ", runtime.GOMAXPROCS(-1))
 	runtime.GOMAXPROCS(100)
-	fmt.Println("Threads : ", runtime.GOMAXPROCS(-1))
-
+	for i := 0; i < 10; i++ {
+		wg.Add(2)
+		m.RLock()
+		go sayHello()
+		m.Lock()
+		go increment()
+	}
+	wg.Wait()
 }
-
-/*
-Threads : 4
-Threads : 100
-*/
+func sayHello() {
+	fmt.Println("Hello #",counter)
+	m.RUnlock()
+	wg.Done()
+}
+func increment(){
+	counter ++
+	m.Unlock()
+	wg.Done()
+}
